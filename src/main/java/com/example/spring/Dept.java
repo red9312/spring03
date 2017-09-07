015f68f79703d873aa22346fbff1655f6b60f9c9
java <java@bigdata.com>
2017-09-07 11:11:54 +0900

    lombok apply

--- src/main/java/com/example/spring/Dept.java
+++ src/main/java/com/example/spring/Dept.java
@@ -1,27 +1,19 @@
 package com.example.spring;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor	// Default 생성자 없이 생성자를 만드는것이다.
+@NoArgsConstructor	// Default 생성자를 만드는것이다.
 public class Dept {
 	int deptno;
 	String dname;
 	String loc;
-	
-	public int getDeptno() {
-		return deptno;
-	}
-	public void setDeptno(int deptno) {
-		this.deptno = deptno;
-	}
-	public String getDname() {
-		return dname;
-	}
-	public void setDname(String dname) {
-		this.dname = dname;
-	}
-	public String getLoc() {
-		return loc;
-	}
-	public void setLoc(String loc) {
-		this.loc = loc;
-	}
 	
 }
